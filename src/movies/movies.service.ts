dba98df5d2d2cf4c67321d599cf7d1cc22df3124
sysmae <j970330@gmail.com>
2025-06-28 00:35:51 +0900

    feat: implement movie management functionality with DTOs and service layer

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: number): Movie {
+    const movie = this.movies.find((movie) => movie.id === id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found`);
+    }
+    return movie;
+  }
+
+  deleteOne(id: number): void {
+    this.getOne(id); // 존재 여부 확인
+    this.movies = this.movies.filter((movie) => movie.id !== id);
+  }
+
+  create(movieData: CreateMovieDto): Movie {
+    const newMovie = new Movie();
+    Object.assign(newMovie, {
+      ...movieData,
+      id: this.movies.length + 1, // 간단한 ID 생성 로직
+    });
+    this.movies.push(newMovie);
+    return newMovie;
+  }
+
+  update(id: number, updateData: UpdateMovieDto): Movie {
+    const movie = this.getOne(id);
+    this.deleteOne(id); // 기존 영화 삭제
+    const updatedMovie = new Movie();
+    Object.assign(updatedMovie, movie, updateData);
+    this.movies.push(updatedMovie); // 업데이트된 영화 추가
+    return updatedMovie;
+  }
+}
